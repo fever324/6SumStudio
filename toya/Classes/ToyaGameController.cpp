2cad33f82dba6a79a989d5017e8928671a87f68a
Hongfei Li <fever324@gmail.com>
2016-05-02 19:47:07 -0400

    bug fix

--- toya/Classes/ToyaGameController.cpp
+++ toya/Classes/ToyaGameController.cpp
@@ -466,7 +466,7 @@ void GameController::update(float dt) {
         
         return;
     }
-    if (_pretime == 0) {
+    if (!_overview->didPause() && !_overview->didHelp() &&_pretime == 0) {
         _theWorld->runFollow();
     }
     
